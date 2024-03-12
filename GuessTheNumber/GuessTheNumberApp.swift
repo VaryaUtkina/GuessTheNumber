987783dfe0f56d1d52a68b6ab1338d99d94f22e7
Varya Utkina <varya2412@mail.ru>
2024-03-12 09:25:41 +0300

    Initial Commit

---
+++ GuessTheNumber/GuessTheNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheNumberApp.swift
+//  GuessTheNumber
+//
+//  Created by Варвара Уткина on 12.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
