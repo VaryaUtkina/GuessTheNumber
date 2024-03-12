4a4e2046bfd511ef8da4a1196a566201eec5706e
Varya Utkina <varya2412@mail.ru>
2024-03-12 13:00:08 +0300

    Create functions for buttons

--- GuessTheNumber/ContentView.swift
+++ GuessTheNumber/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     @State private var currentValue = 50.0
     @State private var targetValue = Int.random(in: 0...100)
+    @State private var isPresented = false
     
     var body: some View {
         VStack(spacing: 35) {
-            Text("\(currentValue)")
             HStack(spacing: 3) {
                 Text("Подвиньте слайдер как можно ближе к:")
                 Text(targetValue.formatted())
@@ -27,11 +27,22 @@ struct ContentView: View {
                     .frame(width: 30)
             }
             VStack(spacing: 20) {
-                Button("Проверь меня!", action: {} )
-                Button("Начать заново", action: {} )
+                Button("Проверь меня!", action: showAlert )
+                    .alert("Your Score", isPresented: $isPresented) {
+                        Button("OK") {}
+                    } message: {
+                        Text(computeScore().formatted())
+                    }
+                Button("Начать заново") {
+                    targetValue = Int.random(in: 0...100)
+                }
             }
         }
         .padding()
+    }
+    
+    private func showAlert() {
+        isPresented.toggle()
     }
     
     private func computeScore() -> Int {
