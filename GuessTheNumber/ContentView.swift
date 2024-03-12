38ba40ad09b6d281ad19fe88baeeab456e558de5
Varya Utkina <varya2412@mail.ru>
2024-03-12 09:42:24 +0300

    Work with ContentView

--- GuessTheNumber/ContentView.swift
+++ GuessTheNumber/ContentView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sliderValue = 50.0
+    @State private var targetValue = Int.random(in: 0...100)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 35) {
+            HStack(spacing: 3) {
+                Text("Подвиньте слайдер как можно ближе к:")
+                Text(targetValue.formatted())
+                    .frame(width: 30, alignment: .leading)
+            }
+            HStack {
+                Text("0")
+                    .frame(width: 30)
+                Slider(value: $sliderValue, in: 0...100)
+                Text("100")
+                    .frame(width: 30)
+            }
+            VStack(spacing: 20) {
+                Button("Проверь меня!", action: {} )
+                Button("Начать заново", action: {} )
+            }
         }
         .padding()
     }
